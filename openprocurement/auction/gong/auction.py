f1e88222e7608f3c9c5c55452bcf771a9b191354
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-08-08 19:03:56 +0300

    Functionality for preparing auction and posting announce

--- openprocurement/auction/gong/auction.py
+++ openprocurement/auction/gong/auction.py
@@ -1,4 +1,7 @@
 import logging
+import sys
+
+from copy import deepcopy
 
 from urlparse import urljoin
 from datetime import datetime
@@ -20,9 +23,12 @@ from openprocurement.auction.gong.journal import (
     AUCTION_WORKER_SERVICE_START_AUCTION,
     AUCTION_WORKER_SERVICE_STOP_AUCTION_WORKER,
     AUCTION_WORKER_SERVICE_PREPARE_SERVER,
-    AUCTION_WORKER_SERVICE_END_FIRST_PAUSE
+    AUCTION_WORKER_SERVICE_END_FIRST_PAUSE,
+    AUCTION_WORKER_API_AUCTION_CANCEL,
+    AUCTION_WORKER_API_AUCTION_NOT_EXIST
 )
 from openprocurement.auction.executor import AuctionsExecutor
+from openprocurement.auction.utils import get_tender_data
 from openprocurement.auction.worker_core.constants import TIMEZONE
 from openprocurement.auction.gong.mixins import\
     DBServiceMixin,\
@@ -32,6 +38,14 @@ from openprocurement.auction.worker_core.mixins import (
     RequestIDServiceMixin,
     AuditServiceMixin,
     DateTimeServiceMixin
+)
+from openprocurement.auctions.gong.utils import (
+    filter_bids,
+    set_bids_information
+)
+from openprocurement.auction.gong.constants import (
+    MULTILINGUAL_FIELDS,
+    ADDITIONAL_LANGUAGES
 )
 
 
@@ -91,6 +105,7 @@ class Auction(DBServiceMixin,
         self.features = None
         self.mapping = {}
         self.rounds_stages = []
+        self.use_api = False
 
     def schedule_auction(self):
         pass
@@ -134,3 +149,151 @@ class Auction(DBServiceMixin,
 
     def post_audit(self):
         pass
+
+    def post_announce(self):
+        if not self.use_api:
+            return
+
+        self.auction_document = self.get_auction_document()
+
+        auction = self.get_auction_data()
+
+        bids_information = filter_bids(auction)
+        set_bids_information(self, self.auction_document, bids_information)
+
+        self.generate_request_id()
+        self.save_auction_document()
+
+    def prepare_auction_document(self):
+        self.generate_request_id()
+        public_document = self.get_auction_document()
+
+        self.auction_document = {}
+        if public_document:
+            self.auction_document = {"_rev": public_document["_rev"]}
+        if self.debug:
+            self.auction_document['mode'] = 'test'
+            self.auction_document['test_auction_data'] = deepcopy(
+                self._auction_data
+            )
+
+        self.synchronize_auction_info(prepare=True)
+
+        self._prepare_auction_document_data()
+
+        if self.worker_defaults.get('sandbox_mode', False):
+            self._prepare_auction_document_stages(fast_forward=True)
+        else:
+            self._prepare_auction_document_stages()
+
+        self.save_auction_document()
+
+    def _prepare_auction_document_data(self):
+        self.auction_document.update({
+            "_id": self.auction_doc_id,
+            "stages": [],
+            "auctionID": self._auction_data["data"].get("auctionID", ""),
+            "procurementMethodType": self._auction_data["data"].get(
+                "procurementMethodType", "default"),
+            "TENDERS_API_VERSION": self.worker_defaults["resource_api_version"],
+            "current_stage": -1,
+            "current_phase": "",
+            "results": [],
+            "procuringEntity": self._auction_data["data"].get(
+                "procuringEntity", {}
+            ),
+            "items": self._auction_data["data"].get("items", []),
+            "value": self._auction_data["data"].get("value", {}),
+            "initial_value": self._auction_data["data"].get(
+                "value", {}
+            ).get('amount'),
+            "auction_type": "kadastral",
+        })
+
+        for key in MULTILINGUAL_FIELDS:
+            for lang in ADDITIONAL_LANGUAGES:
+                lang_key = "{}_{}".format(key, lang)
+                if lang_key in self._auction_data["data"]:
+                    self.auction_document[lang_key] = self._auction_data["data"][lang_key]
+            self.auction_document[key] = self._auction_data["data"].get(
+                key, ""
+            )
+
+    def _prepare_auction_document_stages(self, fast_forward=False):
+        if fast_forward:
+            pass
+        else:
+            pass
+
+    def synchronize_auction_info(self, prepare=False):
+        if self.use_api:
+            self._set_auction_data(prepare)
+
+        self._set_start_date()
+        self._set_bidders_data()
+        self._set_mapping()
+
+    def _set_auction_data(self, prepare=False):
+        # Get auction from api and set it to _auction_data
+        if not self.debug:
+            if prepare:
+                self._auction_data = get_tender_data(
+                    self.tender_url,
+                    request_id=self.request_id,
+                    session=self.session
+                )
+            else:
+                self._auction_data = {'data': {}}
+
+            auction_data = get_tender_data(
+                self.tender_url + '/auction',
+                user=self.worker_defaults["resource_api_token"],
+                request_id=self.request_id,
+                session=self.session
+            )
+
+            if auction_data:
+                self._auction_data['data'].update(auction_data['data'])
+                self.startDate = self.convert_datetime(
+                    self._auction_data['data']['auctionPeriod']['startDate']
+                )
+                del auction_data
+            else:
+                self.get_auction_document()
+                if self.auction_document:
+                    self.auction_document["current_stage"] = -100
+                    self.save_auction_document()
+                    LOGGER.warning("Cancel auction: {}".format(
+                        self.auction_doc_id
+                    ), extra={"JOURNAL_REQUEST_ID": self.request_id,
+                              "MESSAGE_ID": AUCTION_WORKER_API_AUCTION_CANCEL})
+                else:
+                    LOGGER.error("Auction {} not exists".format(
+                        self.auction_doc_id
+                    ), extra={
+                        "JOURNAL_REQUEST_ID": self.request_id,
+                        "MESSAGE_ID": AUCTION_WORKER_API_AUCTION_NOT_EXIST
+                    })
+                    self._end_auction_event.set()
+                    sys.exit(1)
+
+    def _set_start_date(self):
+        self.startDate = self.convert_datetime(
+            self._auction_data['data'].get('auctionPeriod', {}).get('startDate', '')
+        )
+
+    def _set_bidders_data(self):
+        self.bidders_data = [
+            {
+                'id': bid['id'],
+                'date': bid['date'],
+                'owner': bid.get('owner', '')
+            }
+            for bid in self._auction_data['data'].get('bids', [])
+            if bid.get('status', 'active') == 'active'
+        ]
+
+    def _set_mapping(self):
+        for index, bid in enumerate(self.bidders_data):
+            if bid['id'] not in self.mapping:
+                self.mapping[self.bidders_data[index]['id']] = len(self.mapping.keys()) + 1
