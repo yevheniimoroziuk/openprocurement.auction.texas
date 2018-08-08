7b358b5b2a2cd41c5f89cf9361165cb248cf5e1a
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-08-08 18:59:29 +0300

    Add utils functions to get available bids and update auction document with bids info

--- openprocurement/auction/gong/utils.py
+++ openprocurement/auction/gong/utils.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from copy import deepcopy
+
 def prepare_bid_stage(exist_stage_params, params={},  current_auction_value=0):
     # Should get amount of previous stage or initial_auction_value
     # current_auction_value += minimalStep
@@ -6,13 +8,34 @@ def prepare_bid_stage(exist_stage_params, params={},  current_auction_value=0):
     return stage
 
 
-def prepare_auction_document(auction):
-    pass
-
-
 def post_results_data(auction):
     pass
 
 
-def announce_results_data(auction, results):
-    pass
+def prepare_public_document(self):
+    public_document = deepcopy(dict(self.auction_document))
+    return public_document
+
+
+def filter_bids(results):
+
+    bids_information = dict([
+        (bid["id"], bid.get("tenderers"))
+        for bid in results["data"].get("bids", [])
+        if bid.get("status", "active") == "active"
+    ])
+
+    return bids_information
+
+
+def set_bids_information(self, auction_document, bids_information):
+    for field in ['results', 'stages']:
+        for index, stage in enumerate(auction_document[field]):
+            if 'bidder_id' in stage and stage['bidder_id'] in bids_information:
+                auction_document[field][index].update({
+                    "label": {
+                        'uk': bids_information[stage['bidder_id']][0]["name"],
+                        'en': bids_information[stage['bidder_id']][0]["name"],
+                        'ru': bids_information[stage['bidder_id']][0]["name"],
+                    }
+                })
