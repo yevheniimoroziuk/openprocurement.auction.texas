2bd99f6956c177301d92f6c7cd19a81769930847
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-08-10 14:43:30 +0300

    Add constant for auction

--- openprocurement/auction/gong/constants.py
+++ openprocurement/auction/gong/constants.py
@@ -1,2 +1,13 @@
-BIDS_SECONDS = 180
+ENGLISH_DURATION = 180
+PAUSE_DURATION = 10
 BIDS_KEYS_FOR_COPY = ("bidder_id", "time")
+
+MULTILINGUAL_FIELDS = ["title", "description"]
+ADDITIONAL_LANGUAGES = ["ru", "en"]
+
+PRESTARTED = 'pre-started'
+ENGLISH = 'english'
+AUCTION_DEADLINE = 'auction-deadline'
+END = 'announcement'
+
+DEADLINE_TIME = 17
