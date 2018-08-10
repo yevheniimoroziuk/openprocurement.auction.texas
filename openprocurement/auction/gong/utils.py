21f31a9cfcf33a370ea5ba966a27d093ee368aa0
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-08-10 14:45:31 +0300

    Rename utils functions. Add context manager function

--- openprocurement/auction/gong/utils.py
+++ openprocurement/auction/gong/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from copy import deepcopy
+from contextlib import contextmanager
+
 
 def prepare_bid_stage(exist_stage_params, params={},  current_auction_value=0):
     # Should get amount of previous stage or initial_auction_value
@@ -17,7 +19,7 @@ def prepare_public_document(self):
     return public_document
 
 
-def filter_bids(results):
+def get_result_info(results):
 
     bids_information = dict([
         (bid["id"], bid.get("tenderers"))
@@ -28,7 +30,7 @@ def filter_bids(results):
     return bids_information
 
 
-def set_bids_information(self, auction_document, bids_information):
+def set_result_info(auction_document, bids_information):
     for field in ['results', 'stages']:
         for index, stage in enumerate(auction_document[field]):
             if 'bidder_id' in stage and stage['bidder_id'] in bids_information:
@@ -39,3 +41,21 @@ def set_bids_information(self, auction_document, bids_information):
                         'ru': bids_information[stage['bidder_id']][0]["name"],
                     }
                 })
+
+
+@contextmanager
+def update_auction_document(auction):
+    yield auction.get_auction_document()
+    if auction.auction_document:
+        auction.save_auction_document()
+
+
+@contextmanager
+def lock_bids(auction):
+    auction.bids_actions.acquire()
+    yield
+    auction.bids_actions.release()
+
+
+def prepare_audit():
+    pass
