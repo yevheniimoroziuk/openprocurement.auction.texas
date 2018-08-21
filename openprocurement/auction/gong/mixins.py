535a7d3a42c4b943e4c8da683c77ebeef986110f
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-08-21 15:13:58 +0300

    Remove AuctionAPIServiceMixin because its logic has gone to DataSource

--- openprocurement/auction/gong/mixins.py
+++ openprocurement/auction/gong/mixins.py
@@ -8,17 +8,12 @@ from datetime import timedelta, datetime
 from openprocurement.auction.gong.scheduler import SCHEDULER
 
 from openprocurement.auction.utils import (
-    get_tender_data,
-    get_latest_bid_for_bidder,
-    make_request,
     generate_request_id
 )
 from openprocurement.auction.worker_core.constants import TIMEZONE
 from openprocurement.auction.worker_core.utils import prepare_service_stage
 
 from openprocurement.auction.gong.utils import (
-    get_result_info,
-    set_result_info,
     set_specific_hour,
     get_round_ending_time
 )
@@ -30,9 +25,6 @@ from openprocurement.auction.gong.constants import (
     PAUSE_DURATION,
     DEADLINE_HOUR
 )
-from openprocurement.auction.worker_core.mixins import (
-    AuditServiceMixin
-)
 from openprocurement.auction.gong.journal import (
     AUCTION_WORKER_DB_GET_DOC,
     AUCTION_WORKER_DB_GET_DOC_ERROR,
@@ -40,8 +32,6 @@ from openprocurement.auction.gong.journal import (
     AUCTION_WORKER_DB_SAVE_DOC,
     AUCTION_WORKER_DB_SAVE_DOC_ERROR,
     AUCTION_WORKER_DB_SAVE_DOC_UNHANDLED_ERROR,
-    AUCTION_WORKER_API_APPROVED_DATA,
-    AUCTION_WORKER_API_AUCTION_RESULT_NOT_APPROVED,
     AUCTION_WORKER_SERVICE_END_BID_STAGE,
     AUCTION_WORKER_SERVICE_START_NEXT_STAGE,
 )
@@ -193,88 +183,6 @@ class BiddersServiceMixin(object):
 
     def approve_bids_information(self):
         pass
-
-
-class AuctionAPIServiceMixin(
-    AuditServiceMixin
-):
-    auction_doc_id = ''
-    with_document_service = False
-    tender_url = ''
-    api_token = ''
-    session = None
-
-    def put_auction_data(self, auction_data, auction_document):
-        """
-        :param auction_data: data from api
-        :param auction_document: data from auction module couchdb
-        :return: True if auctions result was successfully patched or None if smth wrong
-        """
-
-        if self.with_document_service:
-            doc_id = self.upload_audit_file_with_document_service()
-        else:
-            doc_id = self.upload_audit_file_without_document_service()
-
-        results = self.post_results_data(auction_data)
-
-        if results:
-            bids_information = get_result_info(results)
-            set_result_info(auction_document, bids_information)
-
-            if doc_id and bids_information:
-                self.approve_audit_info_on_announcement(approved=bids_information)
-                if self.with_document_service:
-                    self.upload_audit_file_with_document_service(doc_id)
-                else:
-                    self.upload_audit_file_without_document_service(doc_id)
-
-                return True
-        else:
-            LOGGER.info(
-                "Auctions results not approved",
-                extra={"JOURNAL_REQUEST_ID": request_id,
-                       "MESSAGE_ID": AUCTION_WORKER_API_AUCTION_RESULT_NOT_APPROVED}
-            )
-
-    def post_results_data(self, auction_data, auction_document):
-        """
-        :param auction_data: data from api
-        :param auction_document: data from auction module couchdb
-        :return: response from api where data is posted
-        """
-        result_bids = deepcopy(auction_document["results"])
-        posted_result_data = deepcopy(auction_data["data"]["bids"])
-
-        for index, bid_info in enumerate(auction_data["data"]["bids"]):
-            if bid_info.get('status', 'active') == 'active':
-                auction_bid_info = get_latest_bid_for_bidder(result_bids, bid_info["id"])
-                posted_result_data[index]["value"]["amount"] = auction_bid_info["amount"]
-                posted_result_data[index]["date"] = auction_bid_info["time"]
-
-        data = {'data': {'bids': posted_result_data}}
-        LOGGER.info(
-            "Approved data: {}".format(data),
-            extra={"JOURNAL_REQUEST_ID": request_id,
-                   "MESSAGE_ID": AUCTION_WORKER_API_APPROVED_DATA}
-        )
-        return make_request(
-            self.tender_url + '/auction', data=data,
-            user=self.api_token,
-            method='post',
-            request_id=request_id, session=self.session
-        )
-
-    def get_auction_data(self):
-        request_id = generate_request_id()
-        results = get_tender_data(
-            self.tender_url,
-            user=self.api_token,
-            request_id=request_id,
-            session=self.session
-        )
-
-        return results
 
 
 class StagesServiceMixin(object):
