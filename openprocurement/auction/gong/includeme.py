b8dd5599b812a92cb9f756232d854e697225992c
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2018-08-29 18:31:46 +0300

    Add aliase support

--- openprocurement/auction/gong/includeme.py
+++ openprocurement/auction/gong/includeme.py
@@ -5,8 +5,10 @@ from openprocurement.auction.interfaces import IAuctionsServer
 from .constants import AUCTION_SUBPATH
 
 
-def kadastralProcedure(components):
-    _register(components, 'kadastralProcedure')
+def kadastralProcedure(components, procurement_method_types):
+    for procurementMethodType in procurement_method_types:
+        _register(components, procurementMethodType)
+
     server = components.queryUtility(IAuctionsServer)
     server.add_url_rule(
         rule='/{}/<auction_doc_id>/<path:path>'.format(AUCTION_SUBPATH),
