d483060ba269faa79bbfaf9487d7ae14e5f351ca
Oleksiy Veretiuk <oleksiy.veretiuk@quintagroup.org>
2018-09-04 18:30:57 +0300

    Add opportunity to simple plan auction

--- openprocurement/auction/texas/tests/functional/main.py
+++ openprocurement/auction/texas/tests/functional/main.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import tempfile
 import contextlib
+import yaml
 from datetime import datetime, timedelta
 from dateutil.tz import tzlocal
+from uuid import uuid4
 
 from gevent import monkey
 monkey.patch_all()
@@ -10,11 +12,15 @@ monkey.patch_all()
 import os.path
 import json
 from gevent.subprocess import check_output, sleep
+from openprocurement.auction.utils import calculate_hash
 
 
 PAUSE_SECONDS = timedelta(seconds=120)
 PWD = os.path.dirname(os.path.realpath(__file__))
 CWD = os.getcwd()
+TEST_FILE = '{}/src/openprocurement.auction.texas/openprocurement/auction/texas/tests/functional/data/auction_file.json'.format(CWD)
+
+HASH_SECRET = yaml.load(open('{0}/etc/auction_worker_texas.yaml'.format(CWD), 'r'))['HASH_SECRET']
 
 
 @contextlib.contextmanager
@@ -24,28 +30,35 @@ def update_auctionPeriod(path):
     new_start_time = (datetime.now(tzlocal()) + PAUSE_SECONDS).isoformat()
 
     data['data']['auctionPeriod']['startDate'] = new_start_time
-
-    with tempfile.NamedTemporaryFile(delete=False) as auction_file:
+    with open(TEST_FILE, 'w') as auction_file:
         json.dump(data, auction_file)
-        auction_file.seek(0)
     yield auction_file.name
     auction_file.close()
 
 
 def run_texas(tender_file_path):
     with open(tender_file_path) as _file:
-        auction_id = json.load(_file).get('data', {}).get('id')
+        auction_json = json.load(_file).get('data', {})
+        auction_id = uuid4().hex
+        bids = auction_json.get('bids', [])
         if auction_id:
             with update_auctionPeriod(tender_file_path) as auction_file:
                 check_output(TESTS['texas']['worker_cmd'].format(CWD, auction_id, auction_file).split())
-    sleep(1)
+
+    for bid in bids:
+        print 'texas-auctions/{}/login?bidder_id={}&hash={}'.format(
+            auction_id,
+            bid["id"],
+            calculate_hash(bid["id"], HASH_SECRET)
+        )
+    sleep(30)
 
 
 TESTS = {
     "texas": {
         "worker_cmd": '{0}/bin/auction_texas planning {1}'
                       ' {0}/etc/auction_worker_texas.yaml'
-                      ' --planning_procerude partial_db --auction_info {2}',
+                      ' --planning_procerude partial_db',
         "runner": run_texas,
         'auction_worker_defaults': 'auction_worker_defaults:{0}/etc/auction_worker_defaults.yaml',
         'suite': PWD
